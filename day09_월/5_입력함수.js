dfac96fdf5eb134bd05cc2a84e2c2113a4007415
MSI <kgs2072@naver.com>
2025-06-09 05:26:02 +0900

    test

---
+++ day09_월/5_입력함수.js
@@ -0,0 +1,15 @@
+
+// // 2. JS 입력 관련 기능/이벤트/함수 
+// // [1] confirm()
+// confirm('[7] 확인 메시지 창') // 브라우저에 (확인/취소) 알림 메시지 창( 확인=true , 취소=false )
+// // [2] prompt()
+// prompt('[8] 내용 입력 메시지 창') // 브라우저 (메시지입력) 알림 메시지 창 
+// // [3] document.querySelector('마크업명').innerHTML
+// document.querySelector('body').innerHTML
+
+
+// - 변수와 입출력 활용 
+// - 출력함수 
+
+
+// - 입력함수 
